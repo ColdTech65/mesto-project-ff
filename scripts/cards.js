c3788b8dd0eaeaebb7efd3859b0119c02705e2c7
Alexander <alromanov65@yandex.ru>
2024-12-14 15:41:37 +1100

    Let's get it

--- scripts/cards.js
+++ scripts/cards.js
@@ -8,10 +8,6 @@ const initialCards = [
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
     },
     {
-      name: "Иваново",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-    },
-    {
       name: "Камчатка",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
     },
